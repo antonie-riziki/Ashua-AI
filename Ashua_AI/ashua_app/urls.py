4b38048d32d7c0cd72ee6697c51131a05183dfb5
chelsea_cedric <53366054+antonie-riziki@users.noreply.github.com>
2025-09-16 10:17:43 +0000

    updated the projects settings, urls, and views

---
+++ Ashua_AI/ashua_app/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from . import views
+# from django.contrib.auth.views import LoginView,LogoutView
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('', views.dashboard, name='dashboard'),
+
+]
--- Ashua_AI/ashua_app/views.py
+++ Ashua_AI/ashua_app/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, request
 
 # Create your views here.
+def home(request):
+    return render(request, 'index.html')
+
+
+def dashboard(request):
+    return render(request, 'dashboard.html')
